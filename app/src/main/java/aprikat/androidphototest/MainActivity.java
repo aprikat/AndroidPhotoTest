9a3a1eb75c5be2e04d736fd1952aa27698d9c53c
aprikat <april.y.luo@gmail.com>
2015-11-05 14:47:26 -0800

    basic camera usage

--- app/src/main/java/aprikat/androidphototest/MainActivity.java
+++ app/src/main/java/aprikat/androidphototest/MainActivity.java
@@ -1,5 +1,8 @@
 package aprikat.androidphototest;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,15 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements PhotoOptionsFragment.PhotoOptionsDialogListener{
 
+    private ImageView mImageView;
     private FloatingActionButton photoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.imageViewPhotoTaken);
 
         photoButton = (FloatingActionButton) findViewById(R.id.fab);
         photoButton.setOnClickListener(new View.OnClickListener() {
@@ -57,10 +64,33 @@ public class MainActivity extends AppCompatActivity implements PhotoOptionsFragm
     @Override
     public void onTakePhotoClick(DialogFragment dialog) {
         System.out.println("called onTakePhotoClick");
+        dispatchTakePictureIntent();
     }
 
     @Override
     public void onUploadPhotoClick(DialogFragment dialog) {
         System.out.println("called onUploadPhotoClick");
+    }
+
+    /**
+     * CAMERA STUFF
+     */
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
